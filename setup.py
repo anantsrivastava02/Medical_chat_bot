7bcc7f63ded07597140f0d20ce2be3bb9b04f30b
anantsrivastava02 <itanart333@gmail.com>
2024-07-11 11:21:21 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GENAI_project",
+    version='0.0.0',
+    author='anant srivastava',
+    author_email='anantsrivastava02@gmial.com',
+    packages=find_packages(),
+    install_requires = []
+)
